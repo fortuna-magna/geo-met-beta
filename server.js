448dc1fd0f149c04296d3b599c384d8f674f15e0
fortuna.magna <fortunamagna@tuta.io>
2025-02-17 13:53:44 +1000

    Updated backend to use dynamic port

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
-console.log('Geo-Met backend running');
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3000; // Railway will assign a port dynamically
+
+app.get('/', (req, res) => {
+    res.send('Geo-Met backend running');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
